b7d95c22b6e338c8ff9870b2f9505b6e84eebeee
willworks <scnuzhonghanjin@gmail.com>
2015-11-03 17:54:54 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,7 @@ router.route("/register").get(function(req,res){    // 到达此路径则渲染r
 /* GET home page. */
 router.get("/home",function(req,res){ 
 	if(!req.session.user){ 					//到达/home路径首先判断是否已经登录
-		req.session.error = "请先登录"
+		req.session.error = "请先登录";
 		res.redirect("/login");				//未登录则重定向到 /login 路径
 	}
 	res.render("home",{title:'Home'});         //已登录则渲染home页面
