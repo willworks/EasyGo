b2ce30df38afe937312d5cbc88507fabbea1110b
willworks <scnuzhonghanjin@gmail.com>
2015-12-09 15:55:56 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,37 @@
  * github     : https://github.com/willworks 
  * Description: gulp自动化重载页面
  * Copyright (c) 2015 Kevin Zhong
-
- * 切换淘宝镜像加速
- * npm install -g nrm
- * nrm use taobao
- * chrome安装LiveReload https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
-
  */
 
 //引入插件
 var gulp = require('gulp'),//引入基础库
-    connect = require('gulp-connect');//gulp-connect为gulp插件，用于启动一个Web Server
+    tinylr = require('tiny-lr'),//livereload
+    server = tinylr(),
+    port = 35729,
+    livereload = require('gulp-livereload'),//livereload用于浏览器自动刷新
+    webserver = require('gulp-webserver');//用于在本地启动Http服务
 
-//创建watch任务去检测html文件,其定义了当html改动之后，去调用一个Gulp的Task
-
+//创建watch任务去检测html文件,其定义了当html改动之后，去调用一个gulp的Task
 //以下三个目录要一致，服务器目录，监测目录和重载目录
 gulp.task('watch', function () {
   gulp.watch(['./static/*.html'], ['html']);// 监测目录
 });
 
 //使用connect启动一个Web服务器
-gulp.task('connect', function () {
-  connect.server({
-    root: 'static', //服务器目录
-    port: 8080, //指定端口，默认是8080
-    livereload: true
-  });
+gulp.task('server', function() {
+  gulp.src('./static/')
+    .pipe(webserver({
+      livereload: true,
+      port: 8080, //端口号
+      open: true  //自动打开浏览器
+    }));
+  gulp.start('watch');
 });
 
 gulp.task('html', function () {
   gulp.src('./static/*.html') //重新加载目录
-    .pipe(connect.reload());
+  .pipe(livereload(server));
 });
 
 //运行Gulp时，默认的Task
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['server', 'watch']);
