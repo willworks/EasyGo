0534d7ecf934061e5a64724f5a87cbe27b35af2a
willworks <scnuzhonghanjin@gmail.com>
2015-12-17 19:14:32 +0800

    update controllers

--- app.js
+++ app.js
@@ -32,21 +32,21 @@ global.db = mongoose.connect("mongodb://localhost:27017/EasyGo");
 // ===================登陆校验===================
 // session生命周期设置
 app.use(session({ 
-  secret: 'secret',
-  cookie:{ 
-    maxAge: 1000*60*30
-  }
+    secret: 'secret',
+    cookie:{ 
+        maxAge: 1000*60*30
+    }
 }));
 // session验证登陆
 app.use(function(req,res,next){ 
-  res.locals.user = req.session.user;
-  var err = req.session.error;
-  delete req.session.error;
-  res.locals.message = "";
-  if(err){ 
-    res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">'+err+'</div>';
-  }
-  next();
+    res.locals.user = req.session.user;
+    var err = req.session.error;
+    delete req.session.error;
+    res.locals.message = "";
+    if(err){ 
+        res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">'+err+'</div>';
+    }
+    next();
 });
 // ==============================================
 
@@ -115,10 +115,15 @@ app.use('/notice/:id/delete', noticeCtrl);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+// 通过通配符处理没有经过路由的所有404页面
+app.get('*', function(req, res){
+    res.render("login",{title:'1823619231'});
 });
 
 // error handlers
@@ -126,23 +131,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/departCtrl.js
+++ routes/departCtrl.js
@@ -2,15 +2,111 @@ var express = require('express');
 var router = express.Router();
 
 router.route('/depart').get(function(req,res){
-    if(!req.session.user){
+    if (!req.session.user) {
         req.session.error = "请先登录";
         res.redirect("/login");
+    } else {
+        // 罗列全部内容操作
     }
-    res.render('index', { title: 'depart' });
-    var depart = global.dbConn.getModel('depart');
-    depart.create({ 
-        name: 'depart',
-    }); 
 });
+
+
+router.route("/depart/new").post(function(req,res){ 
+    var User = global.dbConn.getModel('user');
+    var uname = req.body.uname;
+    var upwd = req.body.upwd;
+    User.findOne({name: uname},function(err,data){
+        if(err){ 
+            res.send(500);
+            req.session.error =  '网络异常错误！';
+            console.log(err);
+        }else if(data){ 
+            req.session.error = '用户名已存在！';
+            res.send(500);
+        }else{ 
+            User.create({ 
+                name: uname,
+                password: upwd
+            },function(err,data){ 
+                if (err) {
+                    res.send(500);
+                    console.log(err);
+                } else {
+                    req.session.error = '用户名创建成功！';
+                    res.send(200);
+                }
+            });
+        }
+    });
+});
+
+
+router.route("/depart/:id").get(function(req,res){ 
+    if (!req.session.user) {
+        req.session.error = "请先登录";
+        res.redirect("/login");
+    } else {
+        // 读取具体内容操作
+        var Depart = global.dbConn.getModel('depart');
+        var did = req.body.id;
+        User.findOne({id: id},function(err,data){
+            if(err){ 
+                res.send(500);
+                req.session.error =  '网络异常错误！';
+                console.log(err);
+            }else if(data){ 
+                res.render('index', data);
+                res.send(200);
+            }else{ 
+                req.session.error = '用户不存在！';
+                res.send(200);
+            }
+        });
+    }
+});
+
+router.route("/depart/:id/edit").post(function(req,res){ 
+    if (!req.session.user) {
+        req.session.error = "请先登录";
+        res.redirect("/login");
+    } else {
+        // 编辑具体内容操作
+        var Depart = global.dbConn.getModel('depart');
+        var did = req.body.id;
+        User.findOne({id: id},function(err,data){
+            if(err){ 
+                res.send(500);
+                req.session.error =  '网络异常错误！';
+                console.log(err);
+            }else if(data){ 
+                req.session.error = '用户名已存在！';
+                res.send(500);
+            }else{ 
+                User.create({ 
+                    name: uname,
+                    password: upwd
+                },function(err,data){ 
+                    if (err) {
+                        res.send(500);
+                        console.log(err);
+                    } else {
+                        req.session.error = '用户名创建成功！';
+                        res.send(200);
+                    }
+                });
+            }
+        });
+    }
+});
+
+router.get("/depart/:id/delete",function(req,res){
+    if (!req.session.user) {
+        req.session.error = "请先登录";
+        res.redirect("/login");
+    } else {
+        // 删除操作
+    }
+});
+
 
 module.exports = router;
