44bec84e22303e2cba2aec46e368b58efcec5458
willworks <scnuzhonghanjin@gmail.com>
2015-12-23 14:11:38 +0800

    拆分路由和控制器

--- app.js
+++ app.js
@@ -16,12 +16,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     session = require('express-session'),
-    // controllers
-    mainCtrl = require('./routes/mainCtrl'),
-    userCtrl = require('./routes/userCtrl'),
-    departCtrl = require('./routes/departCtrl'),
-    applyCtrl = require('./routes/applyCtrl'),
-    noticeCtrl = require('./routes/noticeCtrl');
+    routes = require('./routes/routes');
 
 
 // 数据模型
@@ -69,42 +64,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
 
-// ===================设置路由===================
-// PS: :id 可以在 req.params.id获取到
-// 登陆控制
-app.get('/', mainCtrl.index);
-app.post('/login', mainCtrl.login);
-app.get('/logout', mainCtrl.logout);
-
-// 用户控制
-app.get('/user', userCtrl.index);
-app.post('/user/add', userCtrl.add);
-app.get('/user/:id', userCtrl.list);
-app.post('/user/:id/edit', userCtrl.edit);
-app.get('/user/:id/delete', userCtrl.delete);
-
-// 部门控制
-app.get('/depart', departCtrl.index);
-app.post('/depart/add', departCtrl.add);
-app.get('/depart/:id', departCtrl.list);
-app.post('/depart/:id/edit', departCtrl.edit);
-app.get('/depart/:id/delete', departCtrl.delete);
-
-// 申请控制
-app.get('/apply', applyCtrl.index);
-app.post('/apply/add', applyCtrl.add);
-app.get('/apply/:id', applyCtrl.list);
-app.post('/apply/:id/edit', applyCtrl.edit);
-app.get('/apply/:id/delete', applyCtrl.delete);
-
-// 通知控制
-app.get('/notice', noticeCtrl.index);
-app.post('/notice/add', noticeCtrl.add);
-app.get('/notice/:id', noticeCtrl.list);
-app.post('/notice/:id/edit', noticeCtrl.edit);
-app.get('/notice/:id/delete', noticeCtrl.delete);
-
-// ==============================================
+// 路由入口
+routes(app);
 
 
 // 通过通配符处理没有经过路由的所有404页面
---
+++ routes/routes.js
@@ -0,0 +1,45 @@
+// controllers
+var mainCtrl = require('../controllers/mainCtrl'),
+    userCtrl = require('../controllers/userCtrl'),
+    departCtrl = require('../controllers/departCtrl'),
+    applyCtrl = require('../controllers/applyCtrl'),
+    noticeCtrl = require('../controllers/noticeCtrl');
+
+module.exports = function (app) {
+    // ===================设置路由===================
+    // PS: :id 可以在 req.params.id获取到
+    // 登陆控制
+    app.get('/', mainCtrl.index);
+    app.post('/login', mainCtrl.login);
+    app.get('/logout', mainCtrl.logout);
+
+    // 用户控制
+    app.get('/user', userCtrl.index);
+    app.post('/user/add', userCtrl.add);
+    app.get('/user/:id', userCtrl.list);
+    app.post('/user/:id/edit', userCtrl.edit);
+    app.get('/user/:id/delete', userCtrl.delete);
+
+    // 部门控制
+    app.get('/depart', departCtrl.index);
+    app.post('/depart/add', departCtrl.add);
+    app.get('/depart/:id', departCtrl.list);
+    app.post('/depart/:id/edit', departCtrl.edit);
+    app.get('/depart/:id/delete', departCtrl.delete);
+
+    // 申请控制
+    app.get('/apply', applyCtrl.index);
+    app.post('/apply/add', applyCtrl.add);
+    app.get('/apply/:id', applyCtrl.list);
+    app.post('/apply/:id/edit', applyCtrl.edit);
+    app.get('/apply/:id/delete', applyCtrl.delete);
+
+    // 通知控制
+    app.get('/notice', noticeCtrl.index);
+    app.post('/notice/add', noticeCtrl.add);
+    app.get('/notice/:id', noticeCtrl.list);
+    app.post('/notice/:id/edit', noticeCtrl.edit);
+    app.get('/notice/:id/delete', noticeCtrl.delete);
+
+    // ==============================================
+};
