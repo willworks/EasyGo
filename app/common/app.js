df3313bce2f0779d8cc257d553fa60524caf715b
willworks <scnuzhonghanjin@gmail.com>
2016-03-23 12:30:55 +0800

    update filter

--- app/common/app.js
+++ app/common/app.js
@@ -10,8 +10,9 @@ define(function(require, exports, module) {
     require('common/service/authenticationSvc');
     require('common/service/devSvc');
     require('common/directive/confirm');
+    require('common/filter/filter');
 
-    var app = angular.module('app', ['ngRoute', 'angular-lazyload', 'ngAnimate', 'authenticationSvc', 'networkSvc', 'devSvc', 'mgcrea.ngStrap', 'confirm']);
+    var app = angular.module('app', ['ngRoute', 'angular-lazyload', 'ngAnimate', 'authenticationSvc', 'networkSvc', 'devSvc', 'mgcrea.ngStrap', 'confirm', 'filter']);
 
     //注册路由
     //resolve用于客户端的校验，而authenticationSvc.islogin()用于确认服务端的校验情况
--- app/common/filter/filter.js
+++ app/common/filter/filter.js
@@ -8,15 +8,13 @@ define(function(require, exports, module) {
     var filter = angular.module('filter', []);
 
     filter.filter('filter', function(){
-        return {
-            time : function(items){
-                angular.forEach(items,function(item, i){
-                    item = i+'、'+ item + '===';
-                    console.log(item);
-                    items[i] = item;
-                });
-                return items;
-            }
+        return function(items){
+            angular.forEach(items,function(item, i){
+                item = i+'、'+ item + '===';
+                console.log(item);
+                items[i] = item;
+            });
+            return items;
         }
     });
 
